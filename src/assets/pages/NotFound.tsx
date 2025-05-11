a0be2910f181584db298ddca5780458a12463030
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-11 17:34:12 +0200

    feat: add Home and NotFound pages with routing in AppRoutes

---
+++ src/assets/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import { PATHS } from "../router/PATHS"
+import NavbarLink from "../components/NavbarLink"
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center p-5">
+      <h1 className="p-5 text-2xl font-bold">Page not found</h1>
+      <NavbarLink label="Click here to return Home" path={PATHS.home} />
+    </div>
+  )
+}
+
+export default NotFound
--- src/assets/router/AppRoutes.tsx
+++ src/assets/router/AppRoutes.tsx
@@ -1,9 +1,13 @@
-import { Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import { PATHS } from "./PATHS"
+import Home from "../pages/Home"
+import NotFound from "../pages/NotFound"
 
 const AppRoutes = () => {
   return (
     <Routes>
-
+        <Route path={PATHS.home} element={<Home/>}/>
+        <Route path="*" element={<NotFound/>} />
     </Routes>
   )
 }
