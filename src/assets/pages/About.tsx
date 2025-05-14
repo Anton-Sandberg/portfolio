d45ebf4c41a612687ef9f2ebca8963aec2b33a6b
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-14 19:55:43 +0200

    feat: add basic pages
    Home, About, Contact, Projects, Resume, Skills

---
+++ src/assets/pages/About.tsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div className="bg-neutral-900">
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
+    </div>
+  );
+};
+
+export default About;
--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -1,7 +1,6 @@
 const Home = () => {
-  return (
-    <div className="text-center text-4xl">Home</div>
-  )
+  return <div></div>;
+
 }
 
 export default Home
