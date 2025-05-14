684e8a788015f8b59e05cc0f89991379d19698d3
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-14 19:59:36 +0200

    feat: add Navbar component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./assets/components/Navbar";
 import AppRoutes from "./assets/router/AppRoutes";
 import Container from "./assets/components/Container";
 
 function App() {
   return (
     <div className="min-h-screen bg-black text-neutral-500">
+      <Navbar />
       <main>
         <Container>
           <Outlet />
