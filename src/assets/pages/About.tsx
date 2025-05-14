0d417993d300166488ae8cec0738bf3c0d0408df
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-14 20:08:24 +0200

    fix: remove About page placeholder content

--- src/assets/pages/About.tsx
+++ src/assets/pages/About.tsx
@@ -1,17 +1,5 @@
 const About = () => {
-  return (
-    <div className="bg-neutral-900">
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-      <p className="text-white text-center text-9xl">OOOOOOOOOO</p>;
-    </div>
-  );
+  return <div></div>;
 };
 
 export default About;
