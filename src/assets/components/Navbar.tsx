5e544416dca32b87237417b3971628292e825976
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-14 19:59:20 +0200

    feat: implement Navbar component for responsive navigation

---
+++ src/assets/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Container from "./Container";
+import MobileNavbar from "./NavbarMenuMobile.tsx";
+import NavbarMenu from "./NavBarMenu.tsx";
+
+const Navbar = () => {
+  return (
+    <header className="bg-black sticky top-0 z-50 pt-2 pb-2 drop-shadow-[0_4px_10px_rgba(0,0,0,1)]">
+      <Container>
+        <div className="hidden md:block pl-8">
+          <NavbarMenu />
+        </div>
+
+        <div className="block md:hidden">
+          <MobileNavbar />
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Navbar;
