4e08e49a3eee027da4b289dec3d0a0f63b34234b
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-19 17:51:24 +0200

    feat: add PortfolioProject interface for type definition

---
+++ src/types/PortfolioProject.ts
@@ -0,0 +1,10 @@
+export interface PortfolioProject {
+  id: number;
+  name: string;
+  description: string;
+  programmingLanguages: string[];
+  techStacks: string[];
+  dateCompleted: string;
+  githubLink: string;
+  imageUrl: string;
+}
