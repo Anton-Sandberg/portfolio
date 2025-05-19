83a3575b5b4fd2f5edcdb3f1a8e93055c241252d
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-19 20:38:51 +0200

    style: adjust padding and whitespace in WelcomeMessage for improved layout

--- src/components/home-page/WelcomeMessage.tsx
+++ src/components/home-page/WelcomeMessage.tsx
@@ -2,18 +2,19 @@ import Highlight from "@components/common/Highlight";
 
 const WelcomeMessage = () => {
   return (
-    <div className="text-primary flex flex-col items-center text-center text-lg px-4 space-y-6 max-w-2xl mx-auto">
-      <h1 className="text-6xl font-semibold">Hejsan!</h1>
+    <div className="text-primary flex flex-col items-center text-center text-lg space-y-4 max-w-2xl ">
+      <h1 className="text-6xl font-semibold pb-0">Hejsan!</h1>
 
-      <h2 className="text-3xl font-semibold">Välkommen till min portfolio!</h2>
+      <h2 className="text-3xl font-semibold pb-4">Välkommen till min portfolio!</h2>
 
       <p>
-        Jag heter <Highlight>Anton Såndberg</Highlight> och studerar just nu
-        <Highlight>.NET-utveckling</Highlight> på KYH.
+        Jag heter <Highlight className="whitespace-nowrap">Anton Såndberg</Highlight> och
+        studerar <Highlight className="whitespace-nowrap">.NET-utveckling</Highlight> på
+        KYH.
       </p>
 
       <p>
-        Jag söker just nu efter en <Highlight>praktikplats</Highlight> och är taggad på
+        Just nu söker jag efter en <Highlight>praktikplats</Highlight> och är taggad på
         att lära mig ännu mer.
       </p>
 
