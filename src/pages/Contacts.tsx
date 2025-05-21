171efbcf08e1df085ca1c9d4f5d2e136b89d142d
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-22 01:11:05 +0200

    feat: add ContactForm component to Contact page

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,5 +1,11 @@
+import ContactForm from "@/components/contact-page/ContactForm";
+
 const Contact = () => {
-  return <div></div>;
+  return (
+    <section className="max-w-xl mx-auto py-10">
+      <ContactForm />
+    </section>
+  );
 };
 
 export default Contact;
