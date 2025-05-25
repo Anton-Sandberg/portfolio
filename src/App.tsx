357c197a9fd382d1f90bb48c507328e1f4ce5a4e
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-25 20:08:59 +0200

    feat: add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Navbar from "@components/navbar/Navbar";
 import AppRoutes from "@router/AppRoutes";
 import Container from "@components/Container";
+import Footer from "@/components/Footer";
 
 function App() {
   return (
-    <div className="min-h-screen bg-black text-primary font-nunito">
+    <div className="flex flex-col min-h-screen bg-black text-primary font-nunito">
       <Navbar />
-      <main>
+      <main className="grow">
         <Container>
           <AppRoutes />
         </Container>
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-3 mt-4 border-t border-neutral-800">
+      <div className="max-w-7xl mx-auto px-4 grid grid-cols-1 md:grid-cols-3 items-center gap-2 md:gap-0 text-sm text-neutral-500 text-center md:text-left">
+        <p className="md:text-left">Skapad av Anton Såndberg</p>
+
+        <p className="md:text-center">
+          Copyright &copy; {new Date().getFullYear()}
+        </p>
+
+        <div className="md:text-right">
+          <a
+            href="https://github.com/anton-sandberg"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Anton Såndbergs GitHub"
+          >
+            <FaGithub className="inline-block size-6 hover:text-white transition" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
