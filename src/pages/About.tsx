44a4535a1d6e6d53c5f987a6227b20ccb58bb73c
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-26 15:29:06 +0200

    feat: add ProfileFacts and AboutMeAccordion to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,21 @@
+import AboutMeAccordion from "@/components/about-page/AboutMeAccordion";
+import ProfileFacts from "@/components/about-page/ProfileFacts";
+import { FaUser } from "react-icons/fa";
+
 const About = () => {
-  return <div></div>;
+  return (
+    <section className="max-w-3xl mx-auto py-10 flex flex-col md:flex-row justify-center gap-6">
+      <div className="flex justify-center md:self-start">
+        <div className="bg-neutral-900 flex p-4 flex-col justify-between w-full rounded-2xl">
+          <FaUser className="size-50 p-4 self-center" />
+          <ProfileFacts />
+        </div>
+      </div>
+      <div className="flex-1">
+        <AboutMeAccordion />
+      </div>
+    </section>
+  );
 };
 
 export default About;
