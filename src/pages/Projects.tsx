1b53180d30cb1dfdf2bf7e88bf3453b23b73282a
Anton Såndberg <saandberg.anton@gmail.com>
2025-05-27 14:13:05 +0200

    feat: update Projects page with Portfolio Projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,19 @@
+import PortfolioProjectCard from "@/components/portfolio-projects-page/PortfolioProjectCard";
+import { usePortfolioProjects } from "@/hooks/usePortfolioProject";
+
 const Projects = () => {
-  return <div></div>;
+  const result = usePortfolioProjects();
+
+  if (result.error) return <div className="pt-10">Error: {result.error}</div>;
+  if (result.loading) return <div className="pt-10">Loading...</div>;
+
+  return (
+    <div className="grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10">
+      {result.projects.map((project) => (
+        <PortfolioProjectCard key={project.id} project={project} />
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
