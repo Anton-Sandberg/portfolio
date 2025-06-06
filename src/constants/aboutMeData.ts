3c4436c4a637c64dec70ffcf98c9423a953a4778
Anton Såndberg <saandberg.anton@gmail.com>
2025-06-06 19:16:53 +0200

    feat: add profile picture to About page and update content in aboutMeData

--- src/constants/aboutMeData.ts
+++ src/constants/aboutMeData.ts
@@ -9,19 +9,19 @@ export const aboutMeData: AboutMe[] = [
     value: "item-1",
     title: "Personlighet.",
     content:
-      "Jag är relationsinriktad, glad och positiv, open minded, gillar problemlösning - och lite kreativ också, tror jag 😅",
+      "Jag är relationsinriktad, glad och positiv, open minded, gillar problemlösning, och lite kreativ också.",
   },
   {
     value: "item-2",
     title: "Vad jag gör på min fritid.",
     content:
-      "På fritiden gillar jag att utveckla små spelprototyper i Unity och spela datorspel – både för att det är kul och för att få inspiration. Jag umgås gärna med familj, vänner, min sambo och vår hund. Och så försöker jag träna tillräckligt så att kroppen och hjärnan inte blir helt kaputt!",
+      "På fritiden gillar jag att utveckla små spelprototyper i Unity och spela datorspel, både för att det är kul och för att få inspiration. Jag umgås gärna med familj, vänner, min sambo och vår hund. Och så försöker jag träna tillräckligt så att kroppen och hjärnan inte blir helt kaputt!",
   },
 
   {
     value: "item-3",
     title: "Min väg till programmering.",
     content:
-      "2019 blev jag intresserad på att göra egna spel och insåg att jag behövde lära mig programmering. Jag började med Unity och C#, och fastnade direkt – mycket roligare än jag trodde! Jag märkte snabbt att det var just programmeringen jag gillade mest, trots att jag trodde det skulle vara den tråkigaste delen 😂. Detta ledde sedan till att jag sökte .NET-utveckling på KYH.",
+      "2019 blev jag intresserad på att göra egna spel och insåg att jag behövde lära mig programmering. Jag började med Unity och C#, och fastnade direkt, det var mycket roligare än jag trodde! Detta ledde sedan till att jag sökte .NET-utveckling på KYH.",
   },
 ];
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 import AboutMeAccordion from "@/components/about-page/AboutMeAccordion";
 import ProfileFacts from "@/components/about-page/ProfileFacts";
 import TestimonialSection from "@/components/about-page/TestimonialSection";
-import { FaUser } from "react-icons/fa";
+import ProfilePicture from "@assets/images/profile-picture.jpg";
 
 const About = () => {
   return (
@@ -9,7 +9,12 @@ const About = () => {
       <div className="max-w-3xl mx-auto py-10 flex flex-col md:flex-row justify-center gap-6">
         <div className="flex justify-center md:self-start">
           <div className="bg-neutral-900 flex p-4 flex-col justify-between w-full rounded-2xl">
-            <FaUser className="size-50 p-4 self-center" />
+            <img
+              src={ProfilePicture}
+              alt="Profile"
+              className="w-32 object-contain mb-4 rounded-2xl border-4 border-neutral-800 self-center"
+            />
+
             <ProfileFacts />
           </div>
         </div>
